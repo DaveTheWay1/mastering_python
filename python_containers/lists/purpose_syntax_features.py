74d2471c97381f2b710522c76041dc3e414b1355
DaveTheWay1 <ramirezvazquez826@gmail.com>
2024-07-24 22:07:30 -0700

    lists: purpose, syntax, features, and helpful notes for this  portion of python

---
+++ python_containers/lists/purpose_syntax_features.py
@@ -0,0 +1,24 @@
+
+# * Purpose
+# A list provides a container for zero or more items (elements in JS arrays).
+# Lists can contain items of different types, including dictionaries and nested lists.
+
+# * Lists have a class (type) of list.
+
+# * Basic Syntax
+# Like arrays in JS, a list can be created literally with a set of square brackets:
+colors = ['red', 'green', 'blue']
+
+# * The number of items in a list is returned using the built-in len() function:
+len(colors)
+# > 3
+
+# * !! Features
+# Lists are considered to be a sequence type in Python. 
+# A sequence is a generic term used for an ordered collection. 
+# Other sequence types in Python include strings and tuples.
+
+# * Lists are mutable:
+# Existing items within a list can be updated
+# Items can be added to a list
+# Items can be removed from a list
