4ee3a0c514fa26909c6dc7180d51a6f696c962f9
DaveTheWay1 <ramirezvazquez826@gmail.com>
2024-07-24 22:39:26 -0700

    organized files on notes into separate folders: learning and practice. create python practice folder for list and begin lists practice

---
+++ python_containers/lists/practice/lists_practice.py
@@ -0,0 +1,12 @@
+
+# ? Define a list named scores that contains a dictionary with the following shape:
+#  scores = [
+#    {
+#      'name': 'name of the player',
+#      'points': 25  # points the player scored
+#    }
+#  ]
+# Next, using append(), to add an additional “score” dictionary to the scores list.
+# Iterate over the items in the scores list and print a string with this format
+#  <name> scored <points> points
+# for each dictionary item in the list.
