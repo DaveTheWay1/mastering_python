f85e4bf23308a8f61664e7288ae8eb14428679df
DaveTheWay1 <ramirezvazquez826@gmail.com>
2024-07-25 23:04:30 -0700

    how to: iterate through tuples

---
+++ python_containers/tuples/iteration.py
@@ -0,0 +1,10 @@
+
+# * Iteration
+# The items in tuples are iterated over by using 
+# for...in loops as we saw previously with lists:
+colors = ('red', 'green', 'blue')
+for idx, color in enumerate(colors):
+  print(idx, color)
+# > 0 red
+# > 1 green
+# > 2 blue
