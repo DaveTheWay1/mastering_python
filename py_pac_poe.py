85cd6601c0aadbac28afe391202c0b7c1bcb4266
DaveTheWay1 <ramirezvazquez826@gmail.com>
2024-08-12 19:15:35 -0700

    work in progress: current solution for checking all the up and down from a entered move

--- py_pac_poe.py
+++ py_pac_poe.py
@@ -16,73 +16,76 @@ def print_board():
 def change_turns():
     if state['turn'] == 'X':
         state['turn'] = 'Y'
-        # print(f"turn:{state['turn']}")
         return state['turn'] 
     else:
         state['turn'] = 'X'
-        # print(f"turn:{state['turn']}")
         return state['turn']
+
     
 def check_win(move):
-    count = 0
     board_spaces_length = len(list(state['board'].items()))
     board_spaces = list(state['board'].items())
-    # print(board_spaces_length)
-    # print(board_spaces)
-    if '1' in move:
-        for item in board_spaces:
-            if move in item:
-                print(f"move is here {item}")
-                listed_move = list(item[0])
-                listed_move[1] = str(int(listed_move[1]) + 1)
-                below_space = "".join(listed_move)
-                print(state['board'][move])
-                print(state['board'][below_space])
-                if state['board'][move] == state['board'][below_space]:
-                    state['winner'] = True
 
-                
-                
-                # num = int(num[0]) + 1
-                # print(num)
-                # # num = 
-                # print(num)
-                # num = str(num)
-                # print(num)
-                # print(f"num type: {type(num)}, num: {num}")
-                # num = int(list(item[0][1])) + 1
-                # num = str(num)
-                # print(num)
-        # print(board_spaces.index(move))
-        # print(f"move: {state['board'][move]}")
+    for space in board_spaces:
+        move_in_board_space_item = space[0]
+        # print(type((list(move_in_board_space_item))[0]))
+        if move == move_in_board_space_item:
 
-        # print('first row')
-        # # print(board_spaces)
-        # for i in (board_spaces[3:6]):
-        #     print(list(i[0]))
-        #     if list(i[0][0]) == list(move[0]):
-        #         print("letter matches")
-        # print(" ")
-    elif '2' in move:
-        print('second row')
-    else:
-        print('third row')
-    # for i in range(len(list(state['board'].items()))):
-    #     print(board_spaces[i])
-
+            # * the work for all the way up and all the way up and all the way down
+            print(f"move: {move}")
+            print(f"move value: {state['board'][move]}")
+            count = 0
+            # if count  == 0:
+            letter = (list(move_in_board_space_item))[0]
+            num = str(int((list(move_in_board_space_item))[1]) + 1)
+            next_space = letter + num
+            print(f"next space down: {next_space}")
+            result = state['board'].get(next_space)
+            print(f"next space value: {result}")
+            if state['board'][move] == result:
+                print("match")
+                count += 1
+                num = str(int((list(move_in_board_space_item))[1]) + 2)
+                next_space = letter + num
+                print(f"next space down: {next_space}")
+                result = state['board'].get(next_space)
+                print(f"next space value: {result}")
+                if state['board'][move] == result:
+                    count += 1
+                    print(count)
+                    print("you win!")
+            else:
+                print("no match")
+                letter = (list(move_in_board_space_item))[0]
+                num = str(int((list(move_in_board_space_item))[1]) - 1)
+                next_space = letter + num
+                print(f"next space up: {next_space}")
+                result = state['board'].get(next_space)
+                print(f"next space value: {result}")
+                if state['board'][move] == result:
+                    print("match")
+                    num = str(int((list(move_in_board_space_item))[1]) - 2)
+                    next_space = letter + num
+                    print(f"next space up: {next_space}")
+                    result = state['board'].get(next_space)
+                    print(f"next space value: {result}")
+                    if state['board'][move] == result:
+                        count += 1
+                        print(count)
+                        print("you win!")
+                else:
+                    print("no match")
+        # else:
+        #     print("")
 
 def get_move():
     letters_and_nums = ['a', 'b', 'c', 1, 2, 3]
     move = input(f"Player {state['turn']}'s Move (example B2):")
-    # print(move)
-    # print(list(move))
     if len(list(move)) != 2 or list(move)[0] not in letters_and_nums or int(list(move)[1]) not in letters_and_nums or state['board'][move] != None:
         print("Bogus move! Try again... not the required length")
         return 
     else:
         state['board'][move] = state['turn']
-        # print(f"{state['board'][move]}")
-        # print(state['board'])
         check_win(move)
         change_turns()
 
@@ -92,7 +95,7 @@ def init_game():
     'a1': None , 'b1': None, 'c1': None,
     'a2': None , 'b2': None, 'c2': None,
     'a3': None , 'b3': None, 'c3': None
-}
+    }
     print(
     '''
     ----------------------
