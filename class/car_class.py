3241dce3372101f28dff28e9583199ce2ec80bf0
DaveTheWay1 <ramirezvazquez826@gmail.com>
2024-08-20 23:02:15 -0700

    create a car class with a start and stop method and an __str__ override method

---
+++ class/car_class.py
@@ -0,0 +1,26 @@
+class Car():
+  def __init__(self, make, model):
+    self.make = make
+    self.model = model
+    self.running = False
+
+  def start(self):
+    self.running = True
+    print(self.running)
+    
+  def stop(self):
+    self.running = False
+    print(self.running)
+
+  def __str__(self):
+    return f"My {self.make} nice. it's a 2.0; not a reguluh"
+
+my_car = Car("honda", "accord")
+print(my_car)
+my_car.start()
+my_car.stop()
+
+# output:
+# My honda nice. it's a 2.0; not a reguluh
+# True
+# False
