edd13b79937fc951d38652f561328c4545397b13
DaveTheWay1 <ramirezvazquez826@gmail.com>
2024-08-20 20:09:50 -0700

    create a lambda function that includes a filter function

--- functions/lambda_functions/lambda_practice.py
+++ functions/lambda_functions/lambda_practice.py
@@ -19,3 +19,11 @@ print(squares)
 # it will instead say: <map object at 0x1028e9d50>
 # however, with the list wrapping, the output is:
 # [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+# * lambda function with map
+my_nums = [1,2,3,4,5,6,7,8,9,10]
+sqrs = filter(lambda x: x%2 == 0, my_nums)
+print(sqrs)
+# all that is true gets put into a list
+# output: [2, 4, 6, 8, 10]
+# without the list wrapping you would get: <filter object at 0x10275e020>
