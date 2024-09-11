26b5a490d7c9755628fd77d12813d3e841830c3e
David Ramirez <davidramirez@Davids-MacBook-Pro.local>
2024-09-11 16:21:23 -0700

    practice using split on sentences to split all words in a sentence turing each into an element of a list

--- strings/useful_string_methods.py
+++ strings/useful_string_methods.py
@@ -1,25 +1,31 @@
-"ace of spades".split(" ")
-# => ['ace', 'of', 'spades']
+print("using split on a sentence found within a string")
+print("ace of spades".split(" "))
+# output: ['ace', 'of', 'spades']
+sentence = "ace of spades"
+print(sentence.split())
+# output: ['ace', 'of', 'spades']
+print(" the above lists all words in a sentence by using split. ")
+print(" ")
 
 # However, this won't work as desired
-"abcd".split("")
+# "abcd".split("")
 # Instead, use the list() function like this:
-list("abcd")
+# list("abcd")
 # => ['a', 'b', 'c', 'd']
 
 # Warning: Raises error if substring not found
-"tesla".index("s")
+# "tesla".index("s")
 # => 2
 
 # Like index, but returns -1 if substring not found
-"tesla".find("x")
+# "tesla".find("x")
 # => -1
 
-"foo".upper()
+# "foo".upper()
 # => "FOO"
 
-"WHY???".lower()
+# "WHY???".lower()
 # => "why???"
 
-"Then I went to the store I like".replace("I", "you")
+# "Then I went to the store I like".replace("I", "you")
 # => 'Then you went to the store you like'
