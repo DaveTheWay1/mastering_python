9945ec815d438cf182aaea6eaab6ee1aa7e3cac0
David Ramirez <davidramirez@Davids-MacBook-Pro.local>
2024-09-13 15:20:46 -0700

    further clarify why dot notation does not work for dictionaries

--- python_containers/dictionaries/learning/getting_and_setting.py
+++ python_containers/dictionaries/learning/getting_and_setting.py
@@ -11,3 +11,10 @@ print(student['name'])
 student['age'] = 24
 print(student['age'])
 print(student)
+
+# ? print(student.name), why doesnt this work?
+# * because it does not evaluate to a key.
+# you would get the error: AttributeError: 'dict' object has no attribute 'name'
+
+# * to further explain: an attribute are variables, methods, and etc. NOT keys of a dictionary.
+# so what we dont use square brackets, it assumes to look for an attribute and not a dictionary key.
