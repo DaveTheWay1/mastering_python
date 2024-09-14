43c9428d07ddd2728e1332259dd9934415976684
David Ramirez <davidramirez@Davids-MacBook-Pro.local>
2024-09-13 21:59:39 -0700

    another way to convert two list to a dict using zip and dict converters

--- python_containers/dictionaries/practice/dict_challenges.py
+++ python_containers/dictionaries/practice/dict_challenges.py
@@ -10,8 +10,20 @@ values = [10, 20, 30]
 list_to_dict = {
 
 }
+
+# our solution
 # ? how can we loop through 2 lists at once?
 # * through using zip like in the below
 for k,v in zip(keys,values):
     list_to_dict[k] = v
 print(list_to_dict)
+
+# given solution:
+keys = ['Ten', 'Twenty', 'Thirty']
+values = [10, 20, 30]
+
+
+res_dict = dict(zip(keys, values))
+print(res_dict)
+# zip immediately takes two iterables like the above and turns them into tuples
+# by applying dict like so - it behaves like a converter of the result turning them all into a dict
