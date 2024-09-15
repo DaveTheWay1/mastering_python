55e902272d2db81f22d629b8b8a9ada4345c6446
David Ramirez <davidramirez@Davids-MacBook-Pro.local>
2024-09-15 12:59:37 -0700

    demonstrate what returns when run a function without a return statement

---
+++ functions/intro.py
@@ -0,0 +1,13 @@
+
+# * Python Function Syntax
+def this_is_a_function(x):
+    pass
+
+# the pass is what we call stubbing it up.
+# essentially, this saves the space if you dont have the code 
+# you want there ready yet.
+
+# ? what gets returned if we run the function without a return statement?
+print(this_is_a_function('a'))
+# output:
+# None
