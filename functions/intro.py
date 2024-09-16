e3f24d0fadd7cb7036085d8dd438c2305f139240
David Ramirez <davidramirez@Davids-MacBook-Pro.local>
2024-09-16 11:32:01 -0700

    apply return statements to function to see the result, and create a function in which another function(callback function) is passed in

--- functions/intro.py
+++ functions/intro.py
@@ -11,3 +11,31 @@ def this_is_a_function(x):
 print(this_is_a_function('a'))
 # output:
 # None
+
+
+# ? what do functions with a "return" return?
+# we get what we return like in the below
+def add(a,b):
+    return a + b
+print(add(1,1))
+# output: 2
+
+def subtract(a,b):
+    return a - b
+print(subtract(3,2))
+# output: 1
+
+# * CALLBACK FUNCTION
+# in this function, we pass in integer args for parameters
+# a and b and a function arguement for the parameter op.
+# * this is what is called a callback function
+def compute(a,b,op):
+    return op(a,b)
+# ! we do not add parenthesis when we pass the function
+# as an arguement. # ! this is because we dont want to CALL
+# the function as soon as we pass it in. 
+# line 31's paranthessis are the calling parenthesis. 
+# think of add and op, those "words" for our understanding,
+# as simply being  replaced.
+print(compute(3,3,add))
+# output: 6
