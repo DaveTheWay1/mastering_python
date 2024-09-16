88f00e33c7e17ba48b01cd3a746afbc1f5cd0aea
David Ramirez <davidramirez@Davids-MacBook-Pro.local>
2024-09-16 11:56:29 -0700

    add review notes to better clarify the purpose of *args

--- functions/args_kawrgs/accepting_varying_nums_of_args.py
+++ functions/args_kawrgs/accepting_varying_nums_of_args.py
@@ -4,19 +4,21 @@
 # * Python’s * Parameter Specifier (*args)
 # Using the * (“star”) specifier in a parameter list 
 # allows us to pass in a varying number of positional arguments into a function:
-
+# ! ITS A TUPLE. everything passed  into the *args results as a TUPLE.
 def fn(*args):
   print( type(args) )
+  print(args)
   for arg in args:
     print(arg)
 
-# fn(1, 2, 'SEI')
-''' Output:
-<class 'tuple'>
-1
-2
-SEI
-'''
+fn(1, 2, 'SEI')
+# output:
+# <class 'tuple'>
+# (1, 2, 'SEI')
+# 1
+# 2
+# SEI
+
 
 def test(*args):
   print(f'''
@@ -40,8 +42,8 @@ def dev_skills(dev_name, *args):
     dev['skills'].append(skill)
   return dev
 
-print(dev_skills('Alex', 'HTML', 'CSS', 'JavaScript', 'Python'))
-# -> {'name': 'Alex', 'skills': ['HTML', 'CSS', 'JavaScript', 'Python']}
+print(dev_skills('David', 'HTML', 'CSS', 'JavaScript', 'Python'))
+# -> {'name': 'David', 'skills': ['HTML', 'CSS', 'JavaScript', 'Python']}
 
 
 # * Or, why not use the list() function to convert the args tuple into a list…
@@ -50,4 +52,4 @@ def dev_skills(dev_name, *args):
   dev = {'name': dev_name, 'skills': list(args)}
   return dev
 
-print(dev_skills('Alex', 'HTML', 'CSS', 'JavaScript', 'Python'))
+print(dev_skills('David', 'HTML', 'CSS', 'JavaScript', 'Python'))
