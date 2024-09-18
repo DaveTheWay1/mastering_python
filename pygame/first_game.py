e0434e8f7147a53e6696138919ee11a2b30e5c19
David Ramirez <davidramirez@Davids-MacBook-Pro.local>
2024-09-17 21:20:06 -0700

    apply a draw rect and display update to create a rectangle surface and appears on window

--- pygame/first_game.py
+++ pygame/first_game.py
@@ -9,6 +9,12 @@ window = pygame.display.set_mode((500, 500))
 # * the line below titles the game ont he window displayed.
 pygame.display.set_caption("First Game")
 
+x = 50
+y = 50
+width = 40
+height = 60 
+vel = 5
+
 # * Set up a flag to keep the window open
 # without it, the window may close immeidately.
 run = True
@@ -21,6 +27,17 @@ while run:
         # Check if the close button is pressed
         if event.type == pygame.QUIT:
             run = False
+    
+    # * first parameter that gets taken will always be the WINDOW
+        #   - everything in pygame is a surface. 
+                #  - so  when we create the circle  we create a surface (the circle)
+                #  which gets placed onto the window
+    # * second parameter is the COLOR (RGB Color)
+    # * third, it takes a RECT. 
+    # ! depending on whether its a rect or another shape, it require 
+    # something other than a rect like a radius for circles
+    pygame.draw.rect(window, (128,0,0), (x, y, width, height))
+    pygame.display.update()
 
 
 # Quit the game properly
