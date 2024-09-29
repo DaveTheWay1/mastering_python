01c432a3f735a601660b3df834787dc7ce2d138a
DaveTheWay1 <ramirezvazquez826@gmail.com>
2024-09-29 09:36:30 -0700

    create another template file and a view function for it with the routing included in main_app.urls.py

--- django/catcollector_project_folder/catcollector/main_app/views.py
+++ django/catcollector_project_folder/catcollector/main_app/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # Define the home view
 def home(request):
     return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
