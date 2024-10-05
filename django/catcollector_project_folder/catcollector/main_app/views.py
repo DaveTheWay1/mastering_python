f9786ab83310bd66e24c2b13fffd8c8e6731cbf9
DaveTheWay1 <ramirezvazquez826@gmail.com>
2024-10-04 17:52:18 -0700

    create a cat folder for organization and render data onto that cat file

--- django/catcollector_project_folder/catcollector/main_app/views.py
+++ django/catcollector_project_folder/catcollector/main_app/views.py
@@ -7,3 +7,13 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def cats_index(request):
+    return render(request, 'cats/index.html', {
+    'cats': cats
+})
+
+cats = [
+    {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
+    {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
+]
