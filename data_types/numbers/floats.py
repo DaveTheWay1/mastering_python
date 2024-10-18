e07a78073f3f76afaafd317b69d7a8bf47cbd574
DaveTheWay1 <ramirezvazquez826@gmail.com>
2024-10-18 11:47:11 -0700

    additional practice regarding float conversion into an int

--- data_types/numbers/floats.py
+++ data_types/numbers/floats.py
@@ -5,10 +5,21 @@
 # Certain built-in features need to be imported
 import math
 pi = math.pi
-pi
-3.141592653589793
-type(pi)
+# pi
+# 3.141592653589793
+print(type(pi))
 # <class 'float'>
 some_float = 25.
-type(some_float)
+print(type(some_float))
 # <class 'float'>
+
+result = int(pi)
+print(result)
+
+float = 3.9
+round = int(float)
+print(f"float: {float}")
+print(f"round: {round}")
+
+# * in summary, when converting a float to an int
+# * it will always round down.
