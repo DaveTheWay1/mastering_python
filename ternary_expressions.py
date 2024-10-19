251757affed9381f2d402b56d3229c69ca76fd34
DaveTheWay1 <ramirezvazquez826@gmail.com>
2024-10-19 15:10:03 -0700

    practice using ternary expressions and further break down their purpose

--- ternary_expressions.py
+++ ternary_expressions.py
@@ -11,3 +11,18 @@
 # PYTHON
 age = 24
 beverage = 'Beer' if age >= 21 else 'Milk'
+
+print("Do you have a bottle opener? Please answer with a yes or no response.")
+do_you = input()
+response = 'Beer me' if do_you.lower() == 'yes' else 'Mmm, come onnn maneee'
+print(response)
+
+
+# ? can a ternary operator in python have an if else? 
+# ! a ternary operator in Python cannot directly contain another 
+# if-else statement within it.
+# * Why? The purpose of a ternary operator:
+# The ternary operator is meant to be a concise way to express a 
+# simple conditional expression in a single line.
+
+# ! if an if else is needed, use a regular if statement and not a ternary
