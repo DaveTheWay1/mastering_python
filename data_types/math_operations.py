b5bb6c7758b941b833cb6eaccbc5ab8cede02655
DaveTheWay1 <ramirezvazquez826@gmail.com>
2024-10-19 15:07:30 -0700

    include a file with python operations

---
+++ data_types/math_operations.py
@@ -0,0 +1,18 @@
+
+# * Math Operations Of Python
+
+# Addition (+)
+# Subtraction (-)
+# Multiplcation (*)
+# Division (/)
+# Modulo (remainder) (%)
+# Exponentiation (**)
+
+# ? How would one be able to force the result of
+# ? a division to be an integer instead of a float?
+
+# You can force the result to be an integer 
+# when dividing two integers using // instead of /
+quotient = 5 // 2
+print(quotient)
+2
