5a74b0888e4b33bcada6ccb5da050bd0d0bb49dd
DaveTheWay1 <ramirezvazquez826@gmail.com>
2024-11-06 14:25:49 -0800

    more notes on str and repr method

--- class/overriding_methods.py
+++ class/overriding_methods.py
@@ -8,6 +8,9 @@ class Dog():
 
     def __str__(self):
         return f'Dog named {self.name} is {self.age} years old'
+    
+    def __repr__(self):
+        return f"Dog(name={self.name!r}, age={self.age!r})"
 
 
 cazador = Dog("Cazador", 7)
@@ -17,6 +20,23 @@ print(cazador)
 # the __str__ method
 # * new output with overriding: 
 # Dog named Cazador is 7 years old
+
+# *** IN SUMMARY OF STR METHOD
+# * before str there is and __repr__() which is what gives us the "non useful" return.
+# * through str, we successfully are able to translate that with ease. 
+# ! however, it is important to note that repr could essentially have done the same, 
+# ! however it'd look slightly different
+# def __repr__(self):
+#         return f"Dog(name={self.name!r}, age={self.age!r})"
+print("repr")
+print(repr(cazador))
+# output:
+# Dog(name='Cazador', age=7)
+
+# ! while both str and repr can be written to return the same results
+# it is best to keep them separate and use them as their intended.
+# * str is for readability for the users while repr is for developers to 
+# receive and unambiguous return and further debug
 
 print(cazador.name, cazador.age)
 cazador.bark()
